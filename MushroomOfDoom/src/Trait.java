382629a6f9e379ae1b49094a2435efa3f7cd6d88
Nick <nmaga@seas.upenn.edu>
2018-11-07 15:29:44 -0500

    Adding trait images and descriptions

--- MushroomOfDoom/src/Trait.java
+++ MushroomOfDoom/src/Trait.java
@@ -1,17 +1,26 @@
 
 public class Trait {
 	String name;
-	String description; // Actual text of the trait?
+	String description; // Actual text of the trait
+	String imgFileName; // Name of image file for this trait
+	
+	// "Dice roll" bonuses
 	int movementBonus = 0;
 	int attackBonus = 0;
+	
+	// Movement advantages
 	boolean waterMovement;
 	boolean jungleMovement;
 	boolean tundraMovement;
+	// Specific Traits
 	boolean nocturnalism;
+	
 	float probability; // Chance that this trait will appear in the deck
 	
-	public Trait(String name, int movBonus, int attBonus, boolean wtrMov, 
-				boolean jglMov, boolean tdrMov, boolean noct, float prob) {
+	public Trait(String name, String description, String imgFileName,
+				 int movBonus, int attBonus, 
+				 boolean wtrMov, boolean jglMov, boolean tdrMov, boolean noct, 
+				 float prob) {
 		this.name = name;
 		this.movementBonus = movBonus;
 		this.attackBonus = attBonus;
--- MushroomOfDoom/src/TraitDeck.java
+++ MushroomOfDoom/src/TraitDeck.java
@@ -4,16 +4,90 @@ public class TraitDeck {
 	
 	public TraitDeck() {
 		traitArray = new Trait[] {
-				new Trait("None", 0, 0, false, false, false, false, .0f),
-				new Trait("Fangs", 0, 1, false, false, false, false, .1f),
-				new Trait("Fur Coat", 0, 0,  false, false, true, false, .1f),
-				new Trait("Fins", 0, 0, true, false, false, false, .1f),
-				new Trait("Sail", 1, 0, false, false, false, false, .1f),
-				new Trait("Hooves", 1, 0, false, false, false, false, .1f)
+				new Trait(
+						"None", "", "",
+						0, 0, 
+						false, false, false, false, 
+						.0f),
+				new Trait(
+						"Fangs", "Adds +1 to attack.", "Trait-Fangs.JPG",
+						0, 1, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Fur Coat", "You can move normally through Fur Coat.", "Trait-FurCoat.JPG",
+						0, 0,  
+						false, false, true, false, 
+						.1f),
+				new Trait(
+						"Fins", "You can move normally through Water.", "Trait-Fins.JPG", 
+						0, 0, 
+						true, false, false, false, 
+						.1f),
+				new Trait(
+						"Sail", "Adds +1 to movement.", "Trait-Sail.JPG", 
+						1, 0, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Hooves", "Adds +1 to movement.", "Trait-Hooves.JPG", 
+						1, 0, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Gills", "You can move normally through Water.", "Trait-Gills.JPG", 
+						0, 0, 
+						true, false, false, false, 
+						.1f),
+				new Trait(
+						"Setae", "You can move normally through Rainforest.", "Trait-Setae.JPG", 
+						0, 0, 
+						false, true, false, false, 
+						.1f),
+				new Trait(
+						"Stinger", "Adds +1 to attack.", "Trait-Stinger.JPG", 
+						0, 1, 
+						false, false, false, false,
+						.1f),
+				new Trait(
+						"Feathers", "Adds +2 to movement.", "Trait-Feathers.JPG", 
+						2, 0, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Nocturnalism", "You are able to move during Night.", "Trait-Nocturnalism.JPG", 
+						0, 0, 
+						false, false, false, true, 
+						.1f),
+				new Trait(
+						"Tail Club", "Adds +2 to attack.", "Trait-TailClub.JPG", 
+						0, 2, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Prehensile Tail", "You can move normally through Rainforest.", "Trait-PrehensileTail.JPG", 
+						0, 0, 
+						false, true, false, false, 
+						.1f),
+				new Trait(
+						"Extra Legs", "Adds +2 to movement.", "Trait-ExtraLegs.JPG", 
+						2, 0, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Claws", "Adds +2 to attack.", "Trait-Claws.JPG", 
+						0, 2, 
+						false, false, false, false, 
+						.1f),
+				new Trait(
+						"Blubber", "You can move normally through Tundra.", "Trait-Blubber.JPG", 
+						0, 0, 
+						false, false, true, false, 
+						.1f),
 		};
 	}
 	
 	public Trait getRandomTrait() {
-		return new Trait("None", 0, 0, false, false, false, false, .0f);
+		return new Trait("None", "", "", 0, 0, false, false, false, false, .0f);
 	}
 }
